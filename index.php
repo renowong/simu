8ed9711a99ebe66e7d0d077b607f80255d480400
renowong@gmail.com <renowong@gmail.com@a2e124e4-255e-a5c1-b612-ce1a3d23d026>
2012-09-07 01:21:19 +0000

    meta encoding

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ include_once("index_top.php");
 ?>
 <html>
 <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
     <title>Simu</title>
     <script type='text/javascript' src='jquery-1.7.2.min.js'></script>
     <script type="application/x-javascript">
